b81f637c5f577e32e4953bc9dcdf354dbfda39f0
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-01 21:51:52 +0300

    Insert Rowdy

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'calm', true);
+VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'calm', true),
+       ('Rowdy', 4, 'M', 'black', 'friendly', true);
+       
