446585023c285643fe520673a4dcb306f11ef04a
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-01 21:53:29 +0300

    Insert Tabitha

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,5 @@
 INSERT INTO bears (name, age, sex, color, temperament, alive)
 VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'calm', true),
-       ('Rowdy', 4, 'M', 'black', 'friendly', true);
+       ('Rowdy', 4, 'M', 'black', 'friendly', true),
+       ('Tabitha', 3, 'F', 'blue', 'quiet', true),
        
