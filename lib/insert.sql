093930f87a7fd337fa203b2307448f18eaa70a85
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-01 21:55:02 +0300

    Insert Wendy

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,5 @@ VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'calm', true),
        ('Sergeant Brown', 7, 'M', 'golden', 'caring', true),
        ('Melissa', 2, 'F', 'black', 'loud', true),
        ('Grinch', 6, 'M', 'grey', 'angry', true),
+       ('Wendy', 8, 'F', 'silver', 'lovely', true),
        
